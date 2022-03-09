3598bb5cacd963ae2063d469ad9616a3acfbec77
burntato <kineticz300@gmail.com>
2022-03-09 10:58:42 +0700

    sync database ke welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Judul Berita</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @forelse ($news as $item)
+                    <div class="card mt-2">
+                        <div class="card-header">{{$item->title}}</div>
 
-                <div class="card-body">
-                    Content Berita
-                </div>
+                        <div class="card-body">
+                            {{$item->content}}
+                        </div>
+                    </div>
+                @empty
+                    <div class="card mt-2">
+                        <div class="card-header">Belum ada data</div>
+
+                        <div class="card-body">
+                            Belum ada konten
+                        </div>
+                    </div>
+                @endforelse
+
             </div>
         </div>
     </div>
-</div>
 @endsection
